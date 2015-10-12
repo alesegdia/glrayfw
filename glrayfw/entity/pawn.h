d7977a04a561a4b95059a62cdb8b6f325b0d5437
Alejandro Seguí <alesegdia@gmail.com>
2015-10-12 23:36:31 +0200

    removing unnecesary comments

--- glrayfw/entity/pawn.h
+++ glrayfw/entity/pawn.h
@@ -21,8 +21,6 @@ public:
 		cml::vector3f actor2pl = transform.position - viewerPos;
 		cml::vector3f rotactor = cml::rotate_vector( cml::vector3f(1,0,0), cml::vector3f(0,-1,0), cml::rad(180.f)+transform.logic_angle );
 		float datAngle = 180 + cml::deg(cml::signed_angle_2D( cml::vector2f(actor2pl[0],actor2pl[2]), cml::vector2f(rotactor[0],rotactor[2]) ));
-		//float deltangle = playerAngle - actor->GetAngleY();
-		//while( deltangle < 0 ) deltangle += 360;
 		int q = (((int)(datAngle+45)) % 360) / 90;
 		int corr[] = { 2,1,3,0 };
 		framex = corr[q]; framey = 2;
