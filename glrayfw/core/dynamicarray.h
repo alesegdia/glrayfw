57f2903dda2a65c540aa219f03f0d0fd9693d221
Alejandro Seguí <alejandro.segui@thebreachstudios.com>
2024-03-27 00:15:11 +0100

    fixing copy and assignment on dynamic array

--- glrayfw/core/dynamicarray.h
+++ glrayfw/core/dynamicarray.h
@@ -30,11 +30,22 @@ protected:
 
 public:
 
-	DynamicArray( size_t initial_size = INITIAL_SIZE )
+	DynamicArray(size_t initial_size = INITIAL_SIZE)
 	{
 		capacity = initial_size;
 		size = 0;
-		data = ((T*)malloc( initial_size * sizeof(T) ));
+		data = ((T*)malloc(initial_size * sizeof(T)));
+	}
+
+	DynamicArray& operator=(const DynamicArray& other)
+	{
+		CopyOther(other);
+		return *this;
+	}
+
+	DynamicArray(const DynamicArray& other)
+	{
+		CopyOther(other);
 	}
 
 	virtual ~DynamicArray( )
@@ -62,7 +73,13 @@ public:
 		return data[size-1];
 	}
 
-	T& operator[]( size_t index )
+	T& operator[](size_t index) const
+	{
+		// no error check
+		return data[index];
+	}
+
+	T& operator[](size_t index)
 	{
 		// no error check
 		return data[index];
@@ -83,7 +100,7 @@ public:
 		return size == 0;
 	}
 
-	size_t Size()
+	size_t Size() const
 	{
 		return size;
 	}
@@ -92,5 +109,18 @@ public:
 	{
 		return data;
 	}
+
+private:
+	void CopyOther(const DynamicArray& other)
+	{
+		capacity = other.capacity;
+		size = other.size;
+		data = ((T*)malloc(capacity * sizeof(T)));
+		for (int i = 0; i < size; i++)
+		{
+			data[i] = other.data[i];
+		}
+	}
+
 
 };
